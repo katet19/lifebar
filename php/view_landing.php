81777a9feaa32a9f7ee3b734d42f2ac2293ace8a
katet19 <jonathan.schoen@gmail.com>
2015-12-18 08:12:02 -0600

    Initial updated landing page changes

--- php/view_landing.php
+++ php/view_landing.php
@@ -10,170 +10,234 @@
 	 
  	</div>
 	
-<!-- 	Hero
-// 	<div class="section no-pad-bot" id="index-banner">
-// 	    <div class="container">
-// 	        <h1 class="text_h center header cd-headline letters type">
-// 	            <span>I </span> 
-// 	            <span class="cd-words-wrapper waiting">
-// 	                <b class="is-visible">&nbsp;play</b>
-// 	                <b>&nbsp;watch</b>
-// 	                <b>&nbsp;critique</b>
-// 	                <b>&nbsp;stream</b>
-// 	                <b>&nbsp;download</b>
-// 	                <b>&nbsp;experience</b>
-// 	            </span>
-// 	            <span style='padding-left:20px;'> games</span>
-// 	        </h1>
-// 	    </div>
-// 	</div>
-// 	<div class="pressbtn-title">Press Any Button To Continue</div>
-// </div> -->
-		
-		<! -- ********** HEADER ********** -->
-	<div id="h">
-		<div class="container">
-      <div class="col m4" style="text-align:left;">
-        <?php DisplayLifeBarLogo(false); ?>
+<div class="landing-lifebar-container">
+      <div class="landing-lifebar-half-circle"></div>
+      <div class="landing-lifebar-bar"></div>
+        <div class="landing-lifebar-circle">
+
+          <span class="landing-L"><strong>L</strong></span>
+          <div class="landing-lifebar-account">
+            <i class="mdi-action-person"></i>
+            <div class="landing-manAvatar"><img src="Images/LandingPage/manfront.png" /></div>
+          </div>
+        </div>
+        <div class="landing-lifebar-text">
+          <div class="box fade-in one"><strong>i</strong></div>
+          <div class="box fade-in two"><strong>f</strong></div>
+          <div class="box fade-in three"><strong>e</strong></div>
+          <div class="box fade-in four">b</div>
+          <div class="box fade-in five">a</div>
+          <div class="box fade-in six">r</div>
       </div>
-			<div class="row" style="margin-bottom: 0px">
-			    <li><div class="btn btn-lg btn-info btn-login">Login</div> <div class="btn btn-lg btn-info btn-register">Sign Up</div></li>
-          <!-- <li><button class="btn btn-lg btn-info btn-register">Sign Up</button></li> -->
-			    <div class="col m10 offset-m1 mt">
-			    	<h5>Visualize and share</h5>
-			    	<h1 class="mb">YOUR LIFE WITH VIDEO GAMES</h1>
-			    </div>
-			    <div class="col m12 mt hidden-xs" style="padding-top:80px">
-			    	<img src="Images/LandingPage/Profile.png" id="landing_profile" class="responsive-img center-align z-depth-1" alt="">
-			    </div>
-			</div>
-		</div><! --/container -->
-	</div><! --/h -->
+      <div class="landing-xp-text"></div>
+    </div>
 
-	<! -- ********** FIRST ********** -->
-	<div id="w">
-		<div class="row nopadding">
-			<div class="col m5 offset-m1 offset-l2 left-align">
-				<h4>Played or Watched</h4>
-				<p>We consume media in many different ways.  Video games are no longer confined to a couch and TV.</p>
-				<p>Share all of your experiences from the first controller you picked up, to the last Let's Play you watched.</p>
-				<p>It all comes together to form your unique gaming profile, along with a personalized <strong>lifebar</strong>.</p>
-				<p class="mt"><a href="#landing-sign-up" class="btn btn-info btn-theme">Get Started Now</a></p>
-			</div>
+  <div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+      <div class="container">
+        <br><br>
+        <h1 class="header center text-white">YOUR LIFE WITH VIDEO GAMES</h1>
+        <div class="row center">
 
-			<div class="col m6 l5" style="padding:0">
-				<img src="Images/LandingPage/XP.png" id="landing_xp" class="responsive-img right z-depth-1" alt="">
-			</div>
-		</div><! --/row -->
-	</div><! --/container -->
+          <h5 class="header col s12 light">Discover, share and visualize gaming as never before</h5>
+        </div>
+        <div class="row center">
+          <a href="#landing-sign-up" id="download-button" class="landing-sign-up btn-large waves-effect waves-light blue">Sign Up Now</a>
+        </div>
+        <br><br>
+
+      </div>
+    </div>
+
+    <div class="parallax"><img src="Images/LandingPage/playstation-controller.jpg" alt="Unsplashed background img 1"></div>
+    <img src="Images/LandingPage/LandingBGsmall.jpg" class="preloadimage" alt="Unsplashed background img 1" >
+
+  </div>
 
 
-	<! -- ********** BLUE SECTION - PICTON ********** -->
-	<div id="picton">
-		<div class="row nopadding">
-			<div class="col m6 l5" style="padding:0">
-				<img src="Images/LandingPage/Discover.png" id="landing_discover" class="responsive-img left z-depth-1" alt="">
-			</div>
-			<div class="col m5 l6 left-align">
-				<h4>Discover New and Old</h4>
-				<p>Check in on the latest releases or simply walk down memory lane.</p>
-				<p>Every experience counts, whether it was 15 minutes ago, or 15 years ago.</p>
-				<p>Easily track upcoming games, while being notified of similar games that you may have played as a child.</p>
-				<p class="mt"><a href="#landing-sign-up"  class="btn btn-white btn-theme">Discover Games Now</a></p>
-			</div>
+  <!-- <div class="container" style="height: 3000px"> -->
+    <div class="section">
 
-		</div><! --row -->
-	</div><! --/Picton -->
+      <!--   Icon Section   -->
+      <section id="landing-one">
+        <div class="landing-monitor"><img src="Images/LandingPage/monitor.png" /></div>
+        <div class="landing-controller"><img src="Images/LandingPage/DualShock3-in-Hand.png" /></div>
+        <div class="landing-manBack"><img src="Images/LandingPage/manback.png" /></div>
 
 
-	<! -- ********** BLUE SECTION - CURIOUS ********** -->
-	<div id="curious">
-		<div class="row nopadding">
-			<div class="col m5 offset-m1 offset-l2 left-align">
-				<h4>Don't Aggregate.  Personalize.</h4>
-				<p>No longer do you need to read an aggregated score from people you know nothing about to inform you about the games you love.</p>
-				<p>Follow friends and popular personalities to see what they think about the last game they played, or maybe even memories from their past.</p>
-				<p>Find similar tastes and watch how it may shape your future experiences.</p>
-				<p class="mt"><a href="#landing-sign-up" class="btn btn-white btn-theme">Find Out What People Are Saying</a></p>
-			</div>
+        <div class="innerText innerS1">
+          <div class="row">
+        <div class="col s12 m12">
+          <div class="card red darken-1">
+            <div class="card-content white-text">
+              <span class="card-title"><h2>Play games.<br><strong>Earn experience!</strong></h2></span>
+              <ul class="landing-features white-text">
+                  <li>Weather you played <b>10 years ago</b> or <b>10 minutes ago</b>.</li>
+                  <li>Write a<b> short blurb</b> about your experience.</li>
+                  <li>Assign a <b>tier relative to other games</b> from that release year.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+          </div>
+      </section>
+      <section id="landing-two">
+        <div class="innerText innerS2">
+          <div class="row">
+        <div class="col s12 m12 white-text">
+          <div class="card red darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">
+            <h2>Watch others play.<br><strong>Earn experience!</strong></h2>
+           </span> 
+            <ul class="landing-features white-text">
+                <li>You gain knowledge by watching <b> others play.</b></li>
+                <li>Watch anything from <b> beta coverage</b> to a <b> complete playthrough.</b></li>
+                <li>All experiences count towards<b> growing your Lifebar</b>.</li>
+            </ul>
+          </div>
+        </div> 
+      </div>
+    </div>
+        </div>
+        <div class="landing-macbook" ><img src="Images/LandingPage/mackbook-pro-retina.png" />
 
-			<div class="col m6 l5" style="padding:0">
-				<img src="Images/LandingPage/Activity.png" id="landing_activity" class="responsive-img right z-depth-1" alt="">
-			</div>
-		</div><! --/row -->
-	</div><! --/curious -->
+        </div>
 
-	<! -- ********** BLUE SECTION - MALIBU ********** -->
-	<div id="malibu">
-		<div class="row nopadding">
-			<div class="col m6 l5" style="padding:0">
-				<img src="Images/LandingPage/Notifications.png" id="landing_notifications" class="responsive-img left z-depth-1" alt="">
-			</div>
-			<div class="col m5 l6 left-align">
-				<h4>Stay Updated with Notifications</h4>
-				<p>Are others having similar experiences than you?  Are there new thoughts regarding an upcoming release you're interested in?</p>
-				<p>Be alerted when someone agrees with you.  Get notifications on upcoming releases.  Read game suggestions based on previous experiences.</p>
-				<p>Leave no stone unturned as we guide you through your ongoing life with video games!</p>
-				<p class="mt"><a href="#landing-sign-up"  class="btn btn-white btn-theme">Share Your Experiences Now</a></p>
-			</div>
+        <div class="landing-gameGif"><img src="Images/LandingPage/fallout4.gif" /></div>
 
-		</div><! --row -->
-	</div><! --/Malibu -->
+        <div class="landing-manFront"><img src="Images/LandingPage/manfront.png" /></div>
+        <div class="landingGraphs">
+          <canvas id="landing-skills" width="400" height="250"></canvas>
+          <canvas id="landing-skills2" width="400" height="250"></canvas><br>
+          <canvas id="landing-skills3" width="400" height="250"></canvas>
+          <canvas id="landing-skills4" width="400" height="250"></canvas>
+        </div>
+      </section>
+      <section id="landing-three">
+
+        <div class="innerText innerS3">
+          <div class="row">
+        <div class="col s12 m12">
+          <div class="card red darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">
+            <h2>Watch the <br><strong>numbers go up!</strong></h2>
+          </span>
+            <ul class="landing-features white-text">
+                <li>As your experience grows <b> so does your knowledge.</b></li>
+                <li>See what <b>franchises and developers</b> you have spent the most time with.</li>
+                <li>Find out what <b>trends define you as a gamer.</b></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
 
 
-	<! -- ********** BLUE SECTION - JELLY ********** -->
-	<!-- <div id="jelly">
-		<div class="row nopadding">
-			<div class="col-md-5 col-md-offset-1 mt">
-				<h4>Don't Wait, Try Us Now</h4>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-				<p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-				<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-				<p class="mt"><button class="btn btn-white btn-theme">Sign Up Now | 14 Days Free</button></p>
-			</div>
+      </section>
 
-			<div class="col-md-6 mt centered">
-				<h1 data-effect="slide-bottom">24</h1>
-				<h3>$ per month</h3>
-			</div>
-		</div><! --/row
-	</div><! --/Jelly -->
+       <section id="landing-four">
 
-	<! -- ********** FOOTER ********** -->
+        <div class="innerText innerS4">
+          <div class="row">
+        <div class="col s12 m12 white-text">
+          <div class="card red darken-1">
+            <div class="card-content white-text">
+              <span class="card-title">
+            <h2>Don't aggregate.  Personalize.</h2>
+          </span>
+            <ul class="landing-features white-text">
+                <li>You can easily<strong> follow your friends and popular personalities.</strong></li>
+                <li>Access over <strong>3,000 critic experiences and counting!</strong></li>
+                <li>Get the scoop on the<strong> latest AAA releases</strong> and <strong>unknown indie darlings.</strong></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
+        <div class="landingMask">
+          <!--<div class="gameGif"><img src="images/fallout4.gif" /></div>-->
+          <div class="landingActivity"><img src="Images/LandingPage/Activity.png" /></div>
+          <div class="landingNotifications"><img src="Images/LandingPage/Notifications.png" /></div>
+        </div>
+
+
+      </section>
+
+      <section id="landing-five" style="height: 800px">
+
+        <div class="innerText innerS5">
+          <div class="col s12 m12">
+            <div class="card red darken-1">
+              <div class="card-content white-text">
+                <span class="card-title">
+            <h2>Stay in the loop.</h2>
+          </span>
+            <ul class="landing-features white-text">
+                <li>We will <strong>suggest games for you</strong> based of your previous experiences.</li>
+                <li>Be notified when a interesting game is <strong>soon to be released.</strong></li>
+                <li>Reieve <strong>1ups</strong> whenever anybody promotes your way of thinking.</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
+
+
+      </section> 
+
+        </div>
+
+
+  <!--  ********** FOOTER ********** -->
 	<div id="landing-sign-up" class="section scrollspy">
-		<div class="container">
-			<div class="row">
-				<div class="col m8 offset-m2 centered">
+    <div class="row center">
+				<div class="col offset-m2 centered" style="width:70%">
 					<h4 class="mb">Sign Up Now to Find Out More.</h4>
 					<form action="//polygonalweave.us8.list-manage.com/subscribe/post?u=4bc595c55268d466c30ef72dd&amp;id=0b032cbe1c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 	    				<div id="mc_embed_signup_scroll">
 	    					<input type="email" name="EMAIL" value="" class="email subscribe-input" id="mce-EMAIL" placeholder="Enter your e-mail address..." required>
 	    					<div style="position: absolute; left: -5000px;"><input type="text" name="b_4bc595c55268d466c30ef72dd_0b032cbe1c" tabindex="-1" value=""></div>
-							<button class='btn btn-lg btn-info btn-sub subscribe-submit' value="Subscribe" name="subscribe" id='mc-embedded-subscribe' type="submit">Yes, Please</button>
+							<button class='blue btn btn-lg btn-info btn-sub subscribe-submit' value="Subscribe" name="subscribe" id='mc-embedded-subscribe' type="submit">Yes, Please</button>
 						</div>
 					</form>
-
 				</div>
+      </div>
+	</div>
 
-			</div><! --/row -->
-		</div><! --/container -->
-	</div><! --/F -->
-	
 
-	<! -- ********** CREDITS ********** -->
-	<div id="c">
-		<div class="container">
-			<div class="row">
-				<div class="col m6 offset-m3 centered">
+	<!-- ********** CREDITS ********** -->
+	<div id="landing-c">
+      <div class="row center">
+				<div class="col offset-m3 centered" style="width:50%">
 					<p>Copyright 2015 | LifeBar.io</p>
 				</div>
-			</div>
-		</div><! --/container -->
-	</div><! --/C -->
+      </div>
+	</div>
+
+
+  <!--  Scripts-->
+
+
+
+
+  <!-- Include ScrollMagic and GSAP plugin -->
+    <script src="http://lifebar.io/js/library/TweenMax.min.js"></script>
+    <script src="http://lifebar.io/js/library/ScrollMagic.js"></script>
+    <script src="http://lifebar.io/js/library/animation.gsap.min.js"></script>
+    <script src="http://lifebar.io/js/landing-main.js"></script>
+    <script src="http://lifebar.io/js/landing-init.js"></script>
+
 	
 	<script>
 		$(document).ready(function(){
     		$('.scrollspy').scrollSpy();
+    		$('.parallax').parallax();
   		});
 	</script>
 		
