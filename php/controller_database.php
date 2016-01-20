c1a3bf149f9d76b28adcb9ede9cc21793fabb905
katet19 <jonathan.schoen@gmail.com>
2016-01-19 20:07:52 -0600

    Minor bug changes and prepping for moving hosting

--- php/controller_database.php
+++ php/controller_database.php
@@ -4,6 +4,7 @@ function Connect($mysqli = null){
 		return $mysqli;
 	}else{
 		$mysqli = new mysqli("localhost", "polygo6_weaving", "dv+kzs3Ek7BH", "polygo6_weave"); // or die('Error: '.mysql_error());
+        //$mysqli = new mysqli("localhost", "lifeba8_appuser", "a41e684b36ea57", "lifeba8_db");
 		if($mysqli->connect_error){ 
 			usleep(50);
 			ConnectTryTwo();
