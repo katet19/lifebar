1aff8c8b497e770febdf931ef1d688be32383688
katet19 <jonathan.schoen@gmail.com>
2016-05-30 07:48:04 -0500

    Update catch phrase

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if($GLOBALS["DownForMaintenance"]  && $_SESSION['logged-in']->_security != 'Admi
 <?php }else{ ?>
 	<!DOCTYPE HTML>
 	<html>
-	<title>Lifebar | Celebrate your life with games</title>
+	<title>Lifebar | Your gaming life</title>
 	<head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<meta http-equiv="X-Frame-Options" content="deny">
