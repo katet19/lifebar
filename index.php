cf1796cf9a170f81d5ea74915456e59cdca5e2d5
rewphus <rcdurant@gmail.com>
2016-11-08 10:59:40 -0600

    corrected conflict ions

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
-<?php require 'includes.php'; 
-
-if($GLOBALS["DownForMaintenance"]  && $_SESSION['logged-in']->_security != 'Admin'){ ?>
+<?php 
+require 'includes.php'; 
+if($GLOBALS["DownForMaintenance"]){ ?> <!--&& $_SESSION['logged-in']->_security != 'Admin'){?>-->
 	<html>
 	<title>Lifebar - Down for Maintenance</title>
 	<head>
@@ -12,26 +12,9 @@ if($GLOBALS["DownForMaintenance"]  && $_SESSION['logged-in']->_security != 'Admi
 	</body>
 	</html>
 <?php }else{ ?>
-	<!DOCTYPE HTML>
-	<html>
+<!DOCTYPE HTML>
+<html>
 	<?php if(isset($_GET['game'])){ 
-<<<<<<< HEAD
-			$game = GetGame($_GET['game']);
-			?>
-			<title>Lifebar | <?php echo $game->_title; ?></title>
-			<?php
-		}else{
-			?>
-			<title>Lifebar | The Gamers Network</title>
-			<?php
-		} 
-	?>
-	<head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-	<meta http-equiv="X-Frame-Options" content="deny">
-	<meta http-equiv="Cache-control" content="public">
-	<meta name="theme-color" content="#D32F2F">
-=======
 		$game = GetGame($_GET['game']);
 		?>
 		<title>Lifebar | <?php echo $game->_title; ?></title>
@@ -47,10 +30,6 @@ if($GLOBALS["DownForMaintenance"]  && $_SESSION['logged-in']->_security != 'Admi
 <meta http-equiv="X-Frame-Options" content="deny">
 <meta http-equiv="Cache-control" content="public">
 <meta name="theme-color" content="#3F51B5">
-<<<<<<< HEAD
->>>>>>> origin/Akuma
-=======
->>>>>>> Akuma
 	<?php if(isset($_GET['game'])){ ?>
 		<meta name="description" content="<?php echo $game->_title." "; 
 				if($game->_developer != ''){ echo "developed by ".$game->_developer." "; }
@@ -58,7 +37,6 @@ if($GLOBALS["DownForMaintenance"]  && $_SESSION['logged-in']->_security != 'Admi
 				?> 
 		">
 		<?php
-<<<<<<< Updated upstream
 	}
 	?>
 
@@ -144,125 +122,8 @@ if($GLOBALS["DownForMaintenance"]  && $_SESSION['logged-in']->_security != 'Admi
 				DisplayGameViaID($_GET['game'], -1);	
 			}
 			?>
-=======
-	}else{ ?>
-		<meta name="Description" content="Discover, Save and Share your played & watched experiences with video games">
-	<?php } ?>
-	<link rel="shortcut icon" href="http://lifebar.io/fav.ico" type="image/x-icon" />
-	<link href="../css/library/materialize.css" rel="stylesheet" type="text/css" />
-	<link href="../css/font/font-awesome.min.css" rel="stylesheet" type="text/css" />
-	<link href="../css/library/webflow.css" rel="stylesheet" type="text/css" />
-	<link href="../css/deployed.css" rel="stylesheet" type="text/css" />
-	<script type="application/ld+json">
-		{
-		  "@context": "http://schema.org",
-		  "@type": "WebSite",
-		  "name": "Lifebar",
-		  "alternateName": "Lifebar.io",
-		  "url": "http://www.lifebar.io",
-		  "potentialAction": {
-		    "@type": "SearchAction",
-		    "target": "http://lifebar.io/#search/{search_term_string}",
-		    "query-input": "required name=search_term_string"
-		  }
-		}
-		</script>
-		<script type="application/ld+json">
-		{
-		  "@context": "http://schema.org",
-		  "@type": "Organization",
-		  "url": "http://www.lifebar.io",
-		  "logo": "http://www.lifebar.io/Images/lifebarlogoforsocial.png"
-		}
-	</script>
-	</head>
-	<body id="applicationContainer">
-		<div id="navigationContainer">
-			<?php DisplayNavigation(); ?>
-		</div>
-	<div id="contentContainer" class="row">
-		<div id="activity" class="col s12 m12 l9 outerContainer" style='padding: 0'>
-			<div id="activityInnerContainer" class="innerContainer" style="top:0; background-color:rgb(237, 236, 236)"></div>
-		</div>
-		<div id="profile" class="col s12 outerContainer" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 75px;position: absolute;  width: 100%;  z-index: 3;">
-			<div id="profileInnerContainer" class="innerContainer" style="top:0; background-color:rgb(237, 236, 236);min-height: 150%;"></div>
-		</div>
-		<div id="profiledetails" class="col s12 outerContainer" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 75px;position: absolute;  width: 100%;  z-index: 3;">
-			<div id="profiledetailsInnerContainer" class="innerContainer" style="top:0; background-color:rgb(237, 236, 236);min-height: 150%;"></div>
-		</div>
-		<div id="discover" class="col s12 outerContainer" style='padding: 0;'>
-			<div id="discoverInnerContainer" class="innerContainer" style="top:0; background-color:rgb(237, 236, 236)"></div>
-		</div>
-		<div id="notifications" class="col s12 m12 l9 outerContainer" style='padding: 0;display:none;'>
-			<div id="notificationsInnerContainer" class="innerContainer" style="top:0; background-color:rgb(237, 236, 236)"></div>
-		</div>
-		<div id="game" class="col s12 m12 l11 outerContainer" style="padding:0;">
-			<div id="gameInnerContainer" class="innerContainer" style="top:0; background-color:rgb(237, 236, 236);">
-				<?php if(isset($_GET['game'])){ 
-					DisplayGameViaID($_GET['game'], -1);	
-				}
-				?>
-			</div>
-		</div>
-		<div id="settings" class="col s12 m12 l9 outerContainer" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 75px;position: absolute;  width: 100%;  z-index: 3;display:none;">
-			<div id="settingsInnerContainer" class="innerContainer" style="top:0; background-color:rgb(237, 236, 236);min-height: 150%;"></div>
-		</div>
-		<div id="admin" class="col s12 m12 l9 outerContainer" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 110px;position: absolute;  width: 100%;  z-index: 3;">
-			<div id="adminInnerContainer" class="innerContainer" style="top:0; background-color:rgb(237, 236, 236);min-height:100%;"></div>
-		</div>
-		<div id="landing" class="col s12 m12 l9 outerContainer" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 0px;position: absolute;  width: 100%;  z-index: 99;">
-			<div id="landingInnerContainer" class="innerContainer" style="top:0; background-color:rgb(237, 236, 236);min-height:100%;"></div>
->>>>>>> Stashed changes
 		</div>
 	</div>
-<<<<<<< HEAD
-	<div id='fb-root'></div>
-	<?php LoadThirdPartyTools(); ?>
-	<?php DisplaySideDrawer(); ?>
-	<?php DisplayLogin(); ?>
-	<?php DisplaySignup(); ?>
-	<?php DisplayPasswordReset(); ?>
-	<?php DisplayUniversalPopUp(); ?>
-	<?php DisplayUniversalBottomSheet(); ?>
-	<?php DisplayBattleProgressSheet(); ?>
-	<?php DisplayUniversalUserPreview(); ?>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script type="text/javascript" src="../js/deployed.js"></script>
-	<script type="text/javascript" src="../js/library/ScrollMagic.js"></script>
-	<script type="text/javascript" src="../js/library/TweenMax.min.js"></script>
-	<script type="text/javascript" src="../js/library/animation.gsap.min.js"></script>
-	<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-=======
-	<div id="gamemini" class="col s12 outerContainer" style="padding:0;">
-		<div id="gameminiInnerContainer" class="modalInnerContainer z-depth-5"></div>
-	</div>
-	<div id="profile" class="col s12 outerContainer outerContainer-slide-out" style="padding:0;">
-		<div id="profileInnerContainer" class="modalInnerContainer z-depth-3"></div>
-	</div>
-<<<<<<< HEAD
-	<div id="activity" class="col s12 outerContainer outerContainer-slide-out" style='padding: 0'>
-		<div id="activityInnerContainer" class="innerContainer"></div>
-	</div>
-	<div id="profiledetails" class="col s12 outerContainer outerContainer-slide-out" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 75px;position: absolute;  width: 100%;  z-index: 3;">
-		<div id="profiledetailsInnerContainer" class="innerContainer" style="min-height: 150%;"></div>
-	</div>
-	<div id="discover" class="col s12 outerContainer outerContainer-slide-out" style='padding: 0;'>
-		<div id="discoverInnerContainer" class="innerContainer"></div>
-	</div>
-	<div id="notifications" class="col s12 outerContainer outerContainer-slide-out" style='padding: 0;display:none;'>
-		<div id="notificationsInnerContainer" class="innerContainer"></div>
-	</div>
-	<div id="settings" class="col s12 outerContainer outerContainer-slide-out" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 75px;position: absolute;  width: 100%;  z-index: 3;display:none;">
-		<div id="settingsInnerContainer" class="innerContainer" style="min-height: 150%;"></div>
-	</div>
-	<div id="admin" class="col s12 outerContainer outerContainer-slide-out" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 110px;position: absolute;  width: 100%;  z-index: 3;">
-		<div id="adminInnerContainer" class="innerContainer" style="min-height:100%;"></div>
-	</div>
-=======
 	<div id="gamemini" class="col s12 outerContainer" style="padding:0;">
 		<div id="gameminiInnerContainer" class="modalInnerContainer z-depth-5"></div>
 	</div>
@@ -287,7 +148,6 @@ if($GLOBALS["DownForMaintenance"]  && $_SESSION['logged-in']->_security != 'Admi
 	<div id="admin" class="col s12 outerContainer outerContainer-slide-out" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 110px;position: absolute;  width: 100%;  z-index: 3;">
 		<div id="adminInnerContainer" class="innerContainer" style="min-height:100%;"></div>
 	</div>
->>>>>>> Akuma
 	<div id="landing" class="col s12 outerContainer outerContainer-slide-out" style="padding:0;display:none;left: 0px;  bottom: 0;  top: 0px;position: absolute;  width: 100%;  z-index: 99;">
 		<div id="landingInnerContainer" class="innerContainer" style="min-height:100%;"></div>
 	</div>
@@ -306,11 +166,8 @@ if($GLOBALS["DownForMaintenance"]  && $_SESSION['logged-in']->_security != 'Admi
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
->>>>>>> origin/Akuma
-
-	ga('create', 'UA-52980217-1', 'auto');
-
-	</script>
-	</body>
-	</html>
+  ga('create', 'UA-52980217-1', 'auto');
+</script>
+</body>
+</html>
 <?php } ?>
