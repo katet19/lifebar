8a65129df245e81619d06e2bc89c4d403daf61ca
katet19 <jonathan.schoen@gmail.com>
2017-04-03 20:39:27 -0500

    Fix for game sliding in out in ranking

--- php/view_ranking.php
+++ php/view_ranking.php
@@ -6,7 +6,7 @@ function DisplayRanking($userid){
     $unrankedlist = GetMyUnrankedList($userid, -1, '', '');
     $count = 1; 
     ?>
-    <div class="row" style='position:absolute;width:100%;'>
+    <div class="row" style='position:relative;width:100%;'>
         <div class="rank-header-container">
         </div>
         <div class="btn-floating btn-large disabled rank-save-btn"><i class="material-icons left" style='font-size:2em;position: relative;top: 0px;'>save</i> Save</div>        
